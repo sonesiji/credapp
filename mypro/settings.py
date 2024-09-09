13d3984c1bfadb237d8d33c4123ea0d8783f8902
Sonesiji <142778067+sonesiji@users.noreply.github.com>
2024-09-09 12:00:59 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m57seh$t$+^=uaa4s-ftyo_^gg8lg^_$48qo$o*d@@*xa^@n1j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
